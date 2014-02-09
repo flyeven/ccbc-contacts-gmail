0e339f37d4da5902ef39020a8ce513b6948ac5ef
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-02-08 21:05:39 -0900

    set up daily job

--- lib/clock.rb
+++ lib/clock.rb
@@ -6,11 +6,20 @@ require './config/environment'
 module Clockwork
 
   Clockwork.configure do |config|
-    config[:sleep_timeout] = 60  # seconds
+    config[:sleep_timeout] = 60 # seconds
   end
 
   every(1.day, 'Update Contacts', at: '00:00') do
     #DynamicContent.delay.refresh
+    Users.where(recurring: true).each do |user|
+      begin
+        Importer.perform_import(user)
+      rescue => e
+        Rails.logger.error("scheduled import for #{user.name} failed.")
+        Rails.logger.error(e.message)
+        Rails.logger.error(e.backtrace.join("\n"))
+      end
+    end
   end
 
 end
