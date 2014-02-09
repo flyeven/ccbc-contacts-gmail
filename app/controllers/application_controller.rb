2f66f7ef979e6555fd0cc8304e8c74c9c24168a3
Marvin Frederickson <marvin.frederickson@gmail.com>
2014-02-08 18:46:11 -0900

    bundle update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   rescue_from Exception do |exception|
     redirect_to :root, alert: "Sorry, we encountered a problem and have returned you to the home page to try again. [" + exception.message + "]"
     Rails.logger.error(exception.inspect)
+    Raisl.logger.error(exception.backtrace.join("\n"))
   end if Rails.env.downcase == "production"
 
   rescue_from ActionController::InvalidAuthenticityToken, with: :timed_out_handler
